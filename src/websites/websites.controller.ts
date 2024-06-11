def44ca2ffe795bdf3e69d4f32f24cb86ec0a70a
Mil0w0 <lhilderal@myges.fr>
2024-06-11 15:18:59 +0200

    adding scripts route for website deployment

--- src/websites/websites.controller.ts
+++ src/websites/websites.controller.ts
@@ -1,93 +1,163 @@
-import {
-  Controller,
-  Get,
-  Query,
-  Post,
-  Body,
-  Param,
-  Delete,
-  Patch,
-  SetMetadata,
-} from "@nestjs/common";
-import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from "@nestjs/swagger";
-import { WebsitesService } from "./websites.service";
-import { CreateWebsiteDto } from "./dto/create-website-dto";
-import { ListWebsitesDto } from "./dto/list-websites.dto";
-import { Website } from "./website.entity";
-import { UpdateWebsiteDto } from "./dto/update-website.dto";
-import { Role } from "../roles/roles.enum";
-import { Roles } from "../roles/roles.decorator";
+import {Body, Controller, Delete, Get, Param, Patch, Post, Query, SetMetadata,} from "@nestjs/common";
+import {ApiBearerAuth, ApiBody, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {WebsitesService} from "./websites.service";
+import {CreateWebsiteDto} from "./dto/create-website-dto";
+import {ListWebsitesDto} from "./dto/list-websites.dto";
+import {Website} from "./website.entity";
+import {UpdateWebsiteDto} from "./dto/update-website.dto";
 
 export const CAN_SKIP_AUTH_KEY = "isPublic";
 export const SkipAuthentication = () => SetMetadata(CAN_SKIP_AUTH_KEY, true);
+
+type ScriptDTO = {
+    name: string;
+    subDomain: string;
+};
 
 @ApiTags("Websites")
 @Controller("websites")
 @ApiBearerAuth("JWT-auth")
 export class WebsitesController {
-  constructor(private readonly websitesServices: WebsitesService) {}
-  @Post("")
-  @SkipAuthentication() //Allow user to do this without already being logged in (smert)
-  @ApiResponse({
-    status: 201,
-    description: "The website has been successfully created.",
-  })
-  @ApiResponse({
-    status: 400,
-    description: "Bad request",
-  })
-  @ApiBody({
-    type: CreateWebsiteDto,
-    description: "Json structure for create websites object",
-  })
-  async create(@Body() createWebsiteDTO) {
-    return this.websitesServices.create(createWebsiteDTO);
-  }
+    constructor(private readonly websitesServices: WebsitesService) {
+    }
 
-  @Get()
-  @ApiResponse({
-    status: 200,
-    description: "The websites has been successfully fetched.",
-  })
-  async findAll(@Query() query: ListWebsitesDto): Promise<Website[]> {
-    return this.websitesServices.findAll(query.limit, query.page, query.userId);
-  }
+    @Post("")
+    @SkipAuthentication() //Allow user to do this without already being logged in (smert)
+    @ApiResponse({
+        status: 201,
+        description: "The website has been successfully created.",
+    })
+    @ApiResponse({
+        status: 400,
+        description: "Bad request",
+    })
+    @ApiBody({
+        type: CreateWebsiteDto,
+        description: "Json structure for create websites object",
+    })
+    async create(@Body() createWebsiteDTO) {
+        return this.websitesServices.create(createWebsiteDTO);
+    }
 
-  @Get(":id")
-  @ApiResponse({
-    status: 200,
-    description: "The user has been successfully fetched.",
-  })
-  async findOne(@Param("id") id: number): Promise<Website> {
-    return this.websitesServices.findOne(id);
-  }
+    @Get()
+    @ApiResponse({
+        status: 200,
+        description: "The websites has been successfully fetched.",
+    })
+    async findAll(@Query() query: ListWebsitesDto): Promise<Website[]> {
+        return this.websitesServices.findAll(query.limit, query.page, query.userId);
+    }
 
-  @Patch(":id")
-  @ApiResponse({
-    status: 200,
-    description: "The user has been successfully updated.",
-  })
-  @ApiResponse({
-    status: 400,
-    description: "Bad request",
-  })
-  async update(
-    @Param("id") id: number,
-    @Body() updateWebsiteDTO: UpdateWebsiteDto,
-  ): Promise<Website> {
-    return this.websitesServices.update(id, updateWebsiteDTO);
-  }
+    @Get(":id")
+    @ApiResponse({
+        status: 200,
+        description: "The user has been successfully fetched.",
+    })
+    async findOne(@Param("id") id: number): Promise<Website> {
+        return this.websitesServices.findOne(id);
+    }
 
-  @Delete(":id")
-  @ApiResponse({
-    status: 200,
-    description: "The user has been successfully deleted.",
-  })
-  @ApiResponse({
-    status: 404,
-    description: "User not found",
-  })
-  async remove(@Param("id") id: number): Promise<Website> {
-    return this.websitesServices.remove(id);
-  }
+    @Patch(":id")
+    @ApiResponse({
+        status: 200,
+        description: "The user has been successfully updated.",
+    })
+    @ApiResponse({
+        status: 400,
+        description: "Bad request",
+    })
+    async update(
+        @Param("id") id: number,
+        @Body() updateWebsiteDTO: UpdateWebsiteDto,
+    ): Promise<Website> {
+        return this.websitesServices.update(id, updateWebsiteDTO);
+    }
+
+    @Delete(":id")
+    @ApiResponse({
+        status: 200,
+        description: "The user has been successfully deleted.",
+    })
+    @ApiResponse({
+        status: 404,
+        description: "User not found",
+    })
+    async remove(@Param("id") id: number): Promise<Website> {
+        return this.websitesServices.remove(id);
+    }
+
+
+    @Post("scripts/domain")
+    @SkipAuthentication() //Allow user to do this without already being logged in (smert)
+    @ApiResponse({
+        status: 201,
+        description: "The website has been successfully created.",
+    })
+    @ApiResponse({
+        status: 400,
+        description: "Bad request",
+    })
+    @ApiBody({
+        type: CreateWebsiteDto,
+        description: "Json structure for create websites object",
+    })
+    async createDomain(@Body() params: ScriptDTO) {
+        return this.websitesServices.deploySubdomain(params);
+    }
+
+    @Post("scripts/confNginx")
+    @SkipAuthentication() //Allow user to do this without already being logged in (smert)
+    @ApiResponse({
+        status: 201,
+        description: "The website has been successfully created.",
+    })
+    @ApiResponse({
+        status: 400,
+        description: "Bad request",
+    })
+    @ApiBody({
+        type: CreateWebsiteDto,
+        description: "Json structure for create websites object",
+    })
+    async createConfNginx(@Body() params: ScriptDTO) {
+        return this.websitesServices.deployNginxConf(params);
+    }
+
+    @Post("scripts/frontDocker")
+    @SkipAuthentication() //Allow user to do this without already being logged in (smert)
+    @ApiResponse({
+        status: 201,
+        description: "The website has been successfully created.",
+    })
+    @ApiResponse({
+        status: 400,
+        description: "Bad request",
+    })
+    @ApiBody({
+        type: CreateWebsiteDto,
+        description: "Json structure for create websites object",
+    })
+    async createFrontEndDOcker(@Body() params: ScriptDTO) {
+        return this.websitesServices.deployFrontendDocker(params);
+    }
+
+    @Post("scripts/apiDocker")
+    @SkipAuthentication() //Allow user to do this without already being logged in (smert)
+    @ApiResponse({
+        status: 201,
+        description: "The website has been successfully created.",
+    })
+    @ApiResponse({
+        status: 400,
+        description: "Bad request",
+    })
+    @ApiBody({
+        type: CreateWebsiteDto,
+        description: "Json structure for create websites object",
+    })
+    async createAPIdocker(@Body() params: ScriptDTO) {
+        return this.websitesServices.deployAPIdocker(params);
+    }
+
+
 }
--- src/websites/websites.service.ts
+++ src/websites/websites.service.ts
@@ -1,90 +1,176 @@
-import {
-  BadRequestException,
-  Injectable,
-  InternalServerErrorException,
-  NotFoundException,
-} from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { Website } from "./website.entity";
-import { CreateWebsiteDto } from "./dto/create-website-dto";
-import { UpdateWebsiteDto } from "./dto/update-website.dto";
-import { WebsitesValidator } from "./dto/websites.validator";
+import {BadRequestException, Injectable, InternalServerErrorException, NotFoundException,} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {Website} from "./website.entity";
+import {CreateWebsiteDto} from "./dto/create-website-dto";
+import {UpdateWebsiteDto} from "./dto/update-website.dto";
+import {WebsitesValidator} from "./dto/websites.validator";
 import {User} from "../users/user.entity";
+import {exec} from "child_process";
+
+type ScriptDTO = {
+    name: string;
+    subDomain: string;
+}
 
 @Injectable()
 export class WebsitesService {
-  constructor(
-    @InjectRepository(Website)
-    private websitesRepository: Repository<Website>,
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
-  ) {}
-
-  async create(website: CreateWebsiteDto): Promise<Website> {
-    try {
-      WebsitesValidator.validateWebsiteDto(website);
-    } catch (error) {
-      throw new BadRequestException(error.message);
+    constructor(
+        @InjectRepository(Website)
+        private websitesRepository: Repository<Website>,
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) {
     }
 
-    const user = await this.usersRepository.findOneBy({id: website.userId});
-    if (!user) {
-      throw new BadRequestException(`User ${website.userId} not found`);
+    async create(website: CreateWebsiteDto): Promise<Website> {
+        try {
+            WebsitesValidator.validateWebsiteDto(website);
+        } catch (error) {
+            throw new BadRequestException(error.message);
+        }
+
+        const user = await this.usersRepository.findOneBy({id: website.userId});
+        if (!user) {
+            throw new BadRequestException(`User ${website.userId} not found`);
+        }
+        website.user = user;
+
+        const otherWebsite = await this.websitesRepository.findOneBy({
+            url: website.url,
+        });
+        if (otherWebsite) {
+            throw new BadRequestException(`URL ${website.url} already exists`);
+        }
+
+        try {
+            //TODO: RUN THE SCRIPT TO DEPLOY AND PERSONALIZE NEW DOCKER
+            return await this.websitesRepository.save(website);
+        } catch (error) {
+            throw new InternalServerErrorException(
+                error.message || "An error occurred while creating the website",
+            );
+        }
     }
-    website.user = user;
 
-    const otherWebsite = await this.websitesRepository.findOneBy({
-      url: website.url,
-    });
-    if (otherWebsite) {
-      throw new BadRequestException(`URL ${website.url} already exists`);
+    async update(id: number, params: UpdateWebsiteDto): Promise<Website> {
+        await this.findOne(id);
+        //TODO: what can we update ?
+        await this.websitesRepository.update(id, params);
+        return await this.websitesRepository.findOneBy({id});
     }
 
-    try {
-      //TODO: RUN THE SCRIPT TO DEPLOY AND PERSONALIZE NEW DOCKER
-      return await this.websitesRepository.save(website);
-    } catch (error) {
-      throw new InternalServerErrorException(
-        error.message || "An error occurred while creating the website",
-      );
+    async findAll(limit?: number, page?: number, userId?: number): Promise<Website[]> {
+
+        if (!userId) {
+            return await this.websitesRepository.find({
+                take: limit || 10,
+                skip: (page - 1) * limit || 0,
+            });
+        } else {
+            return await this.websitesRepository.find({
+                take: limit || 10,
+                skip: (page - 1) * limit || 0,
+                where: {user: {id: userId}},
+            });
+        }
     }
-  }
 
-  async update(id: number, params: UpdateWebsiteDto): Promise<Website> {
-    await this.findOne(id);
-    //TODO: what can we update ?
-    await this.websitesRepository.update(id, params);
-    return await this.websitesRepository.findOneBy({ id });
-  }
-
-  async findAll(limit?: number, page?: number, userId?: number): Promise<Website[]> {
-
-    if (!userId) {
-      return await this.websitesRepository.find({
-        take: limit || 10,
-        skip: (page - 1) * limit || 0,
-      });
-    } else {
-      return await this.websitesRepository.find({
-        take: limit || 10,
-        skip: (page - 1) * limit || 0,
-        where: {user: {id: userId}},
-      });
+    async findOne(id: number): Promise<Website> {
+        const website = await this.websitesRepository.findOneBy({id});
+        if (!website) {
+            throw new NotFoundException(`Website #${id} not found`);
+        }
+        return website;
     }
-  }
 
-  async findOne(id: number): Promise<Website> {
-    const website = await this.websitesRepository.findOneBy({ id });
-    if (!website) {
-      throw new NotFoundException(`Website #${id} not found`);
+    async remove(id: number): Promise<Website> {
+        const website = await this.findOne(id);
+        await this.websitesRepository.delete(id);
+        return website;
     }
-    return website;
-  }
 
-  async remove(id: number): Promise<Website> {
-    const website = await this.findOne(id);
-    await this.websitesRepository.delete(id);
-    return website;
-  }
+    async deploySubdomain(params: ScriptDTO) {
+        const scriptPath = "/root/scripts/new-dommain.sh " + params.name + " " + params.subDomain;
+        let message;
+        exec(`bash ${scriptPath}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing script: ${error.message}`);
+                message = error.message;
+            }
+            if (stderr) {
+                console.error(`Script stderr: ${stderr}`);
+                message = stderr;
+            }
+            console.log(`Script stdout: ${stdout}`);
+            message = stdout;
+        });
+        return {
+            message: message
+        }
+    }
+
+    async deployAPIdocker(params: ScriptDTO) {
+//./root/scripts/deploy-api.sh <name> <subdomain>
+        const scriptPath = "/root/scripts/deploy-api.sh " + params.name + " " + params.subDomain;
+        let message;
+        exec(`bash ${scriptPath}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing script: ${error.message}`);
+                message = error.message;
+            }
+            if (stderr) {
+                console.error(`Script stderr: ${stderr}`);
+                message = stderr;
+            }
+            console.log(`Script stdout: ${stdout}`);
+            message = stdout;
+        });
+        return {
+            message: message
+        }
+    }
+
+
+    async deployFrontendDocker(params: ScriptDTO) {
+        // ./root/scripts/deploy-front.sh <name> <subdomain>
+        const scriptPath = "/root/scripts/deploy-front.sh " + params.name + " " + params.subDomain;
+        let message;
+        exec(`bash ${scriptPath}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing script: ${error.message}`);
+                message = error.message;
+            }
+            if (stderr) {
+                console.error(`Script stderr: ${stderr}`);
+                message = stderr;
+            }
+            console.log(`Script stdout: ${stdout}`);
+            message = stdout;
+        });
+        return {
+            message: message
+        }
+    }
+
+    async deployNginxConf(params: ScriptDTO) {
+//./root/scripts/add-nginx.sh <name> <subdomain>
+        const scriptPath = "/root/scripts/add-nginx.sh " + params.name + " " + params.subDomain;
+        let message;
+        exec(`bash ${scriptPath}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing script: ${error.message}`);
+                message = error.message;
+            }
+            if (stderr) {
+                console.error(`Script stderr: ${stderr}`);
+                message = stderr;
+            }
+            console.log(`Script stdout: ${stdout}`);
+            message = stdout;
+        });
+        return {
+            message: message
+        }
+    }
 }
