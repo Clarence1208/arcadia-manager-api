b3ed5f177f3745d494d9cedb1c9dd66662958f65
Clarence HIRSCH <chirsch1@myges.fr>
2024-06-12 16:28:10 +0200

    adding https to the api

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,44 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import * as fs from "node:fs";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle("Arcadia Solutions Manager API")
-    .setDescription("The Arcadia Solutions manager API")
-    .setVersion("1.0")
-    .addBearerAuth(
-      {
-        type: "http",
-        scheme: "bearer",
-        bearerFormat: "JWT",
-        name: "JWT",
-        description: "Enter JWT token",
-        in: "header",
-      },
-      "JWT-auth", // This name here is important for matching up with @ApiBearerAuth() in your controller!
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+    const httpsOptions = {
+        key: fs.readFileSync('/etc/ssl/private-key.pem'),
+        cert: fs.readFileSync('/etc/ssl/public-certificate.pem'),
+    };
+    const app = await NestFactory.create(
+        AppModule,
+        {httpsOptions}
+    );
 
-  app.enableCors({
+    const config = new DocumentBuilder()
+        .setTitle("Arcadia Solutions Manager API")
+        .setDescription("The Arcadia Solutions manager API")
+        .setVersion("1.0")
+        .addBearerAuth(
+            {
+                type: "http",
+                scheme: "bearer",
+                bearerFormat: "JWT",
+                name: "JWT",
+                description: "Enter JWT token",
+                in: "header",
+            },
+            "JWT-auth", // This name here is important for matching up with @ApiBearerAuth() in your controller!
+        )
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+
+    app.enableCors({
         //Add your origins here
         origin: true,
     });
-  await app.listen(3000);
+    await app.listen(3000);
 
 }
+
 bootstrap();
