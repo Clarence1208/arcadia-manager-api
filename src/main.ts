a2d1c86f94f143552564b1e8e2ff34b54cd51ef6
Nino <ninoplane@gmail.com>
2024-07-20 00:27:44 +0200

    ssh v2

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,13 @@ import * as fs from "node:fs";
 async function bootstrap() {
 
     const httpsOptions = {
-    key: fs.readFileSync('/etc/ssl/live/arcadia-solution.com-0002/privkey.pem'),
-    cert: fs.readFileSync(
-      '/etc/ssl/live/arcadia-solution.com-0002/fullchain.pem',
-    ),
+        key: fs.readFileSync('/etc/ssl/live/arcadia-solution.com-0002/privkey.pem'),
+        cert: fs.readFileSync('/etc/ssl/live/arcadia-solution.com-0002/fullchain.pem'),
     };
 
     const app = await NestFactory.create(
         AppModule,
-        {httpsOptions}
+        {httpsOptions},
     );
 
     const config = new DocumentBuilder()
